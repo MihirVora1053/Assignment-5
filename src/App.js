3e84fe531fc8a74040ed992dfd17840a9de83962
Mihir <mihirvora1053@gmail.com>
2021-08-23 14:03:41 +0530

    Assignment 5 first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./components/header/header";
+import Navbar from "./components/navbar/navbar";
+import Hero from "./components/hero/hero";
+import Banner from "./components/banner/banner";
+import DonateInfo from "./components/donateInfo/donateInfo";
+import Card1 from "./components/card1/card1";
+import Banner2 from "./components/banner2/banner";
+import Chart from "./components/chart/chart";
+import Card2 from "./components/card2/cards";
+import Event from "./components/event/event";
+import Footer from "./components/Footer/footer";
+import Team from "./components/team-slider/slider";
+import Contact from './components/contact/contact';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Navbar />
+      <Hero />
+      <Banner />
+      <DonateInfo />
+      <Card1 />
+      <Chart />
+      <Card2 />
+      <Banner2 />
+      <Contact/>
+      <Team />
+      <Event />
+      <Footer />
+    </>
   );
 }
 
